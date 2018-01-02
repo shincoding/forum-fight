09e356a80e930f2ef67578eff98bdbe47b4fdd81
Jaewon Shin <jaewon.shin@mail.utoronto.ca>
2018-01-01 18:11:39 -0800

    Removed files from gitignore

--- ignored/workingConfig.py
+++
@@ -1,6 +0,0 @@
-username = "forum_fight_bot"
-password = "thisisapassword123"
-client_id = "6u37eKWWXiH9xg"
-client_secret = "aG-Oy_xjXQDFULkRCPyjjRvdI7E"
-
-# conn = pymysql.connect("sql3.freemysqlhosting.net", "sql3187319", "r1VDIBw4lj", "sql3187319")
